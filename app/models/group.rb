4f5b1e168e364abfb7567dd61716c60c637fad54
Oleksandr Papevis <sasha@papevis.com>
2022-09-16 19:49:23 +0200

    group validation and normalisation

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,12 +1,25 @@
+# frozen_string_literal: true
+
 class Group < ApplicationRecord
+  before_validation :strip_whitespace
+  validates :name,
+            allow_blank: false,
+            length: { maximum: 80 },
+            format: { without: /http/, message: "urls are not allowed"}
+
   belongs_to :creator,
              class_name: 'User',
              foreign_key: :creator_id,
              inverse_of: :groups,
-             required: true
+             optional: false
   has_many :group_members,
-           foreign_key: :group_id
+           foreign_key: :group_id,
+           inverse_of: :group
   has_many :members, class_name: 'User', through: :group_members
+
+  def strip_whitespace
+    self.name = name.squish unless name.nil?
+  end
 
   def joinable?(user = nil)
     return false if user.blank?
